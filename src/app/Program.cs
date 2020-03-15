afeaa68e3d34c885fa4633110cf3b68bcecd1a69
Yuuki Wesp <ivysola@0xf6.moe>
2020-03-15 15:39:08 +0300

    add blazor template

---
+++ src/app/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hmac
+{
+    using System.Threading.Tasks;
+    using app;
+    using Microsoft.AspNetCore.Blazor.Hosting;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
