4d497823aa1122ff57e3d7948f2a776b8483cfeb
Yuuki Wesp <ivysola@0xf6.moe>
2020-03-16 20:39:25 +0300

    update

--- src/app/wwwroot/js/@code.js
+++ src/app/wwwroot/js/@code.js
@@ -1,15 +1,30 @@
-﻿function domReady() {
+﻿var guard = null;
+function hookClipboard() {
+    $(".clipboard-copy").click(function () {
+        $('body').toast({
+            title: 'Copied!',
+            message: 'A signature copied to clipboard.',
+            showProgress: 'bottom',
+            classProgress: 'red'
+        });
+        navigator.clipboard.writeText($("#signature").val());
+    });
+}
+function domReady() {
+    if (guard !== null)
+        return;
     window.editor = ace.edit("editor");
     editor.renderer.setShowGutter(false);
     editor.getSession().setUseWorker(false);
     editor.setTheme("ace/theme/chrome");
     editor.getSession().setMode("ace/mode/json");
+    hookClipboard();
+    $('.ui.dropdown').dropdown();
+    guard = "";
 }
-
 function setValue(id, val) {
     $(id).val(val);
 }
-
 function castHMAC(adapter, text, secret) {
     if (adapter === "SHA512")
         return CryptoJS.HmacSHA512(text, secret).toString();
