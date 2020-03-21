491e790a5747e20878bad14ed3f3433e51e6932b
Yuuki Wesp <ivysola@0xf6.moe>
2020-03-21 14:51:28 +0300

    update

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -2,15 +2,20 @@
 {
     using System.Threading.Tasks;
     using app;
-    using hmac.Core;
+    using Blazored.LocalStorage;
+    using Core;
     using Microsoft.AspNetCore.Blazor.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.Add(new Microsoft.Extensions.DependencyInjection.ServiceDescriptor(typeof(HMACProcessor), typeof(HMACProcessor), Microsoft.Extensions.DependencyInjection.ServiceLifetime.Scoped));
+            builder.Services
+                .AddScoped<HMACProcessor, HMACProcessor>()
+                .AddScoped<IToastController, ToastController>()
+                .AddBlazoredLocalStorage();
             builder.RootComponents.Add<App>("app");
 
             await builder.Build().RunAsync();
